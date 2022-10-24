97acd37aa7d4fd86ecfd05022fa60c4ec88e8e1e
Allyson Mitchem <allysonmitchem@gmail.com>
2022-10-24 14:14:21 -0600

    added postBank and map over listing posts

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const morgan = require("morgan");
+const postBank = require('./postBank');
+
 const app = express();
+app.use(morgan('dev'))
 
-app.get("/", (req, res) => res.send("Hello World!"));
+// app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/" , (req, res) => {
+const posts = postBank.list()
 
+const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post => `<li>${post.title
+      } By: ${post.name}`)}</li>
+     
+    </ul>
+  </body>
+</html>`
+
+res.send(html)
+})
+// console.log(post.title)
 const PORT = 1337;
 
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`App listening in port ${PORT}`, );
 });
